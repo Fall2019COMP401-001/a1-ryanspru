dc3f03ad25b9bc0a062df626c7942f99606bb252
ryansprumont <ryansprumont@ryans-air-2.wireless-1x.unc.edu>
2019-08-30 17:35:51 -0400

    Commit 1 for adept

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -7,8 +7,43 @@ public class A1Adept {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
+		
+		int count = scan.nextInt();
+		
+		String[] name = new String[count];
+		double[] price = new double[count];
+		
+		for (int i = 0; i < count; i++) {
+			name[i] = scan.next();
+			price[i] = scan.nextDouble();
+		}
+		
+		int cusCount = scan.nextInt();
+		
+		String[] firstName = new String[cusCount];
+		String[] lastName = new String[cusCount];
+		
+		for (int j = 0; j < cusCount; j++) {
+			firstName[j] = scan.next();
+			lastName[j] = scan.next();
+			
+			int itemCount = scan.nextInt();
+			
+			int[] quantity = new int[itemCount];
+			String[] itemName = new String[itemCount];
+			
+			for (int e = 0; e < itemCount; e++) {
+				quantity[e] = scan.nextInt();
+				itemName[e] = scan.next();
+				
+			}
+		}
+		
+		if itemName.equals()
+		
 
 		// Your code follows here.
 		
 	}
+	
 }
